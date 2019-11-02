efcaea14668ebc9d9413be91e86f0596544ae87e
sfoust1 <sfoust61@gmail.com>
2019-11-01 21:02:47 -0700

    Added LinkedIn Img
    Testing LinkedIn Img

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,9 +8,27 @@ class LandingPage extends Component {
         <Grid className='Landing-grid'>
           <Cell col={12}>
             <img
-              src='../Profile.jpg'
+              src='https://github.com/sfoust1/myportfoliosite/blob/master/Profile.jpg?raw=true'
               alt='avatar'
+              className="avatar-img"
             />
+
+            <div className="banner-text">
+              <h1> Full Stack Web Developer </h1>
+
+              <hr/>
+
+              <p> JavaScript | React | NodeJS | Java | C++ | C | HTML/CSS </p>
+
+              <div className="social-links">
+                <a href="https://www.linkedin.com/in/sfoust1/" rel="noopener noreferrer" target="_blank">
+                  <img
+                    src="https://fontawesome.com/icons/linkedin?style=brands"
+                    alt="linkedIn"
+                  />
+                </a>
+              </div>
+            </div>
           </Cell>
         </Grid>
       </div>
