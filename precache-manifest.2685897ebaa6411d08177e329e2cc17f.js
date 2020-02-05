593115011d6ecd6dfa9b7b61b5660935a858dc00
sfoust1 <sfoust61@gmail.com>
2020-02-04 23:22:30 -0800

    Updates

---
+++ precache-manifest.2685897ebaa6411d08177e329e2cc17f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf2fae11ae436e7f8cecdbe78989906",
+    "url": "/myportfoliosite/index.html"
+  },
+  {
+    "revision": "c72ee6992a73384dea44",
+    "url": "/myportfoliosite/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "8a6e462293a5d643c88b",
+    "url": "/myportfoliosite/static/css/main.a03f62b2.chunk.css"
+  },
+  {
+    "revision": "c72ee6992a73384dea44",
+    "url": "/myportfoliosite/static/js/2.71f116ad.chunk.js"
+  },
+  {
+    "revision": "f365850841c060193bc763eb0abbf866",
+    "url": "/myportfoliosite/static/js/2.71f116ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6e462293a5d643c88b",
+    "url": "/myportfoliosite/static/js/main.7eeb9d13.chunk.js"
+  },
+  {
+    "revision": "b5afb437c6e5e43fffb0",
+    "url": "/myportfoliosite/static/js/runtime-main.1c29a5fe.js"
+  }
+]);
